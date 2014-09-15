97b4109f0fede9046cf42f116646729b457b6362
yangscar <yangscar@gmail.com>
2014-09-15 17:30:11 +0800

    add for Lecture script

---
+++ forMATLAB_Lecture.m
@@ -0,0 +1,45 @@
+%% I Test script
+
+[a,b]=myOperator1('+',1,2,3,4,5)
+[a,b]=myOperator1('*',1,2,3,4,5)
+
+
+[a,b]=myOperator2(@myAdd,1,2,3,4,5)
+
+newAdd = @(x,y)myAdd2(x,y,2)
+
+[a,b]=myOperator2(newAdd,1,2,3,4,5)
+
+
+%% save part
+
+A = [1 2 ;3 4];
+B = 'String';
+save('mySave.mat','A','B');
+clear all;
+load mySave.mat
+A
+B
+A = [A; [5 6]];
+save('mySave.mat','A','-append');
+clear all;
+load mySave.mat
+A
+B
+
+
+x = 0:0.1:4*pi;
+plot(x,sin(x));
+saveas(gca,'myPlot','fig');
+%% Timer part
+
+myTimer = timer();
+myTimer.StartDelay=1;
+myTimer.TimerFcn = @myPrint;
+myTimer.Period = 1;
+myTimer.execution ='fixedRate';
+start(myTimer);
+
+
+stop(myTimer);
+delete(myTimer);
